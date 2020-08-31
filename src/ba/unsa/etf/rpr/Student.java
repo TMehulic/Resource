3fb90326bead425f8c287bf9a892cc9b18650324
tmehulic1 <tmehulic1@etf.unsa.ba>
2020-08-31 22:27:24 +0200

    Created student class

--- src/ba/unsa/etf/rpr/Student.java
+++ src/ba/unsa/etf/rpr/Student.java
@@ -2,13 +2,26 @@ package ba.unsa.etf.rpr;
 
 public class Student {
 
-    private String firstName;
     private String lastName;
-    private String date;
+    private String firstName;
+    private String fathersName;
     private String placeOfBirth;
-    private String currentPlace;
+    private long jmbg;
+    private String phone;   //as string, to easily handle leading zeros in database
+    private String email;
+    private String image;   //path to image
+    //todo: GENDER I DATUM RODJENJA DODATI
+
+    //Prebivaliste
+    private String adress;
     private String canton;
-    private String country;
+    private String county;
+
+    //Fakultet
+
+    private String smjer;
+    private int ciklus;
+    private int godina;
     private int index;
 
 
