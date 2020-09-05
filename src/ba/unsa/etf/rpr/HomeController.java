84957228cb70c04cffd2ad3b9f9876a1f740a8ca
tmehulic1 <tmehulic1@etf.unsa.ba>
2020-09-05 19:47:32 +0200

    Implemented redirection

--- src/ba/unsa/etf/rpr/HomeController.java
+++ src/ba/unsa/etf/rpr/HomeController.java
@@ -4,115 +4,85 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 
-import static javafx.scene.Node.getClassCssMetaData;
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class HomeController {
 
     static int userID;
 
-    public TextField fldUsername;
+    private FXMLLoader loader;
+    private Parent root;
+
+    public TextField fldEmail;
     public PasswordField fldPassword;
     public Button btnLogin;
+    public Label exceptionLabel;
 
 
-    public HomeController() { }
+    public HomeController() {
+        userID=-1;
+    }
 
     @FXML
     public void initialize(){
-        fldUsername.textProperty().addListener((obs,oldName,newName) ->{
 
-        });
-
-        fldPassword.textProperty().addListener((obs, oldPass, newPass) -> {
-
-        });
-
+        exceptionLabel.setText("");
         btnLogin.setOnAction(loginAction);
 
     }
 
     public EventHandler<ActionEvent> loginAction = actionEvent -> {
-        // 1. Check inputs
 
-        // 2. Get person
-
-        // 3. Redirect to dashboard
-
-        // https://stackoverflow.com/questions/14370183/passing-parameters-to-a-controller-when-loading-an-fxml
-
-        //Ako je student
-
-        //poslati student id
-
-//        StudentController ctrl = new StudentController();
-//
-//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/student.fxml"));
-//        loader.setController(ctrl);
-//
-//        Parent root = null;
-//        try {
-//            root = loader.load();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-        //Ako je admin
-
-//        AdminController ctrl = new AdminController();
-//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/admin.fxml"));
-//        loader.setController(ctrl);
-//
-//        Parent root = null;
-//        try {
-//            root = loader.load();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//
-//        //Ako je profesor
-//        ProfessorController ctrl = new ProfessorController();
-//
-//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/professor.fxml"));
-//        loader.setController(ctrl);
-//
-//        Parent root = null;
-//        try {
-//            root = loader.load();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-        System.out.println(fldUsername.textProperty().getValue());
-        System.out.println(fldPassword.textProperty().getValue());
-        if(fldUsername.textProperty().getValue().equals("Adis") && fldPassword.textProperty().getValue().equals("123")){
-            StudentController studentController= new StudentController();
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/student.fxml"));
-
-            loader.setController(studentController);
-
-            Parent root = null;
+        Person user = DAOClass.getInstance().getUser(fldEmail.textProperty().getValue(),fldPassword.textProperty().getValue());
+        if(user==null){
             try {
-                root=loader.load();
-            } catch (IOException e) {
-                e.printStackTrace();
+                throw new InvalidLoginException("Pogrešni pristupni podaci.");
+            } catch (InvalidLoginException e) {
+                exceptionLabel.setText(e.getMessage());
             }
-            Main.getGuiStage().setTitle("Student");
+        }else{
+            userID=user.getId();
+            if(user instanceof Student){
+                StudentController ctrl = new StudentController();
+                loader=new FXMLLoader(getClass().getResource("/fxml/student.fxml"));
+                loader.setController(ctrl);
+                Main.getGuiStage().setTitle("Student");
+                redirectToDashboard();
+            }else if(user instanceof Professor){
+                ProfessorController ctrl = new ProfessorController();
+                loader=new FXMLLoader(getClass().getResource("/fxml/professor.fxml"));
+                loader.setController(ctrl);
+                Main.getGuiStage().setTitle("Professor");
+                redirectToDashboard();
+            }else {
+                try {
+                    throw new InvalidLoginException("Pogrešni pristupni podaci.");
+                } catch (InvalidLoginException e) {
+                    userID=-1;
+                    exceptionLabel.setText(e.getMessage());
+                }
+            }
+        }
+    };
+
+    public void redirectToDashboard(){
+        try {
+            root = loader.load();
             Main.getGuiStage().setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
             Main.getGuiStage().show();
             Main.getGuiStage().setResizable(true);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    };
+    }
 
 }
---
+++ src/ba/unsa/etf/rpr/InvalidLoginException.java
@@ -0,0 +1,7 @@
+package ba.unsa.etf.rpr;
+
+public class InvalidLoginException extends Exception {
+    public InvalidLoginException(String message) {
+        super(message);
+    }
+}
