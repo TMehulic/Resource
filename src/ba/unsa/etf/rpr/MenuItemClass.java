b263282b6a45b74cb820c0194fac3012ba102081
tmehulic1 <tmehulic1@etf.unsa.ba>
2020-09-13 23:36:34 +0200

    Added about fxml

--- src/ba/unsa/etf/rpr/MenuItemClass.java
+++ src/ba/unsa/etf/rpr/MenuItemClass.java
@@ -1,7 +1,16 @@
 package ba.unsa.etf.rpr;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.util.Locale;
 import java.util.ResourceBundle;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class MenuItemClass {
 
@@ -16,7 +25,21 @@ public class MenuItemClass {
     }
 
     public void showAbout(){
-
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("/fxml/about.fxml"),Main.bundle);
+        AboutController ctrl = new AboutController();
+        loader.setController(ctrl);
+        Parent root = null;
+        try {
+            root = loader.load();
+            Stage stage=new Stage();
+            stage.setTitle("About");
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void logOut(){
